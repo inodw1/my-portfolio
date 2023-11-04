22e0f866aa1b2533101b3bc2050f4180f3395f8c
inodw1 <inodwagachchi@gmail.com>
2023-11-04 18:50:14 +0530

    feat: add higher order component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => (
     <Tilt className="xs:w-[250px] w-full">
@@ -47,12 +48,12 @@ const About = () => {
                 variants={fadeIn("", "", 0.1, 1)}
                 className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
             >
-                I'm a skilled software developer with experience in TypeScript
-                and JavaScript, and expertise in frameworks like React, Node.js,
-                and Three.js. I'm a quick learner and collaborate closely with
-                clients to create efficient, scalable, and user-friendly
-                solutions that solve real-world problems. Let's work together to
-                bring your ideas to life!
+                I'm a skilled software developer with experience in JavaScript,
+                and expertise in frameworks like React, Node.js, and Three.js.
+                I'm a quick learner and collaborate closely with clients to
+                create efficient, scalable, and user-friendly solutions that
+                solve real-world problems. Let's work together to bring your
+                ideas to life!
             </motion.p>
 
             <div className="mt-20 flex flex-wrap gap-10">
@@ -68,4 +69,4 @@ const About = () => {
     );
 };
 
-export default About;
+export default SectionWrapper(About, "About");
